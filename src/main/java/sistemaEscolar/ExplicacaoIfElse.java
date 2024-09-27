594c5634e22bf24115124f47c6cf29a6cc6fa79e
Patricia Eliseu <patyeliseu4@gmail.com>
2024-09-27 15:13:32 -0300

    Sistema Escolar Trybe

---
+++ src/main/java/sistemaEscolar/ExplicacaoIfElse.java
@@ -0,0 +1,15 @@
+package sistemaEscolar;
+/*
+resultado seja superior a 7, é exibido a mensagem:
+"PARABÉNS: VOCÊ ESTÁ NA PRÓXIMA ETAPA!"
+
+caso contrario, exibe:
+"POXA, NÃO FOI DESTA VEZ. ATÉ A PRÓXIMA!"
+ */
+
+
+public class ExplicacaoIfElse {
+    public static void main(String[] args) {
+
+    }
+}
