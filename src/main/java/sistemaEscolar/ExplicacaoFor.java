ce29d50f521d01f3125abbead8ec2d12114de22c
Patricia Eliseu <patyeliseu4@gmail.com>
2024-09-27 16:31:53 -0300

    Explicacao For

--- src/main/java/sistemaEscolar/ExplicacaoFor.java
+++ src/main/java/sistemaEscolar/ExplicacaoFor.java
@@ -1,4 +1,25 @@
 package sistemaEscolar;
 
 public class ExplicacaoFor {
+    public static void main(String[] args) {
+        String[] nomes = new String[] {"MARCOS", "ALINE", "LUCAS", "PATRICIA"};
+
+        /*
+        for (int i = 0; i < nomes.length; i++) {
+            imprimeMensagem(nomes[i]);
+        }
+         */
+
+        for (String nome: nomes) {
+            imprimeMensagem(nome);
+        }
+
+
+    }
+
+    static void imprimeMensagem(String mensagem) {
+        System.out.println(mensagem);
+    }
+
+
 }
